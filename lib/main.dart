313fb12eac6cfef736a72348a8cbd46dbf36b715
User <rrk@gmail.com>
2023-03-27 23:49:44 +0600

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:berber_booking_app/screens/phone.dart';
+import 'package:berber_booking_app/screens/verify.dart';
+import 'package:berber_booking_app/state/state_management.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_ui/firebase_auth_ui.dart';
+import 'package:firebase_auth_ui/providers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,14 +20,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final size=MediaQuery.of(context).size;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Berber Shop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: PhoneScreen(),
+      home: const PhoneScreen()
     );
   }
+
+
 }
 
--- lib/screens/phone.dart
+++ lib/screens/phone.dart
@@ -1,3 +1,4 @@
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -9,9 +10,11 @@ class PhoneScreen extends StatefulWidget {
 }
 
 class _PhoneScreenState extends State<PhoneScreen> {
+  bool otpFieldVisibility=false;
   TextEditingController countryCode = TextEditingController();
   var phone = "";
-
+  var receivedID = '';
+  FirebaseAuth auth = FirebaseAuth.instance;
   @override
   void initState() {
     countryCode.text = "+880";
@@ -30,62 +33,58 @@ class _PhoneScreenState extends State<PhoneScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 55,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      SizedBox(
-                        width: 40,
-                        child: TextField(
-                          controller: countryCode,
-                          keyboardType: TextInputType.phone,
-                          decoration:
-                              const InputDecoration(border: InputBorder.none),
-                        ),
-                      ),
-                      const Text('|',style: TextStyle(fontSize: 33,color: Colors.grey),),
-
-                      SizedBox(height: 10,),
-                      Expanded(child: TextField(
-                        onChanged: (value){
-                          phone=value;
-                        },
-                         keyboardType: TextInputType.phone,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Phone",
-
-                        ),
-                      ))
-                    ],
-                  ),
-                )),
+            // Padding(
+            //     padding: const EdgeInsets.all(8.0),
+            //     child: Container(
+            //       height: 55,
+            //       decoration: BoxDecoration(
+            //         border: Border.all(width: 1, color: Colors.grey),
+            //       ),
+            //       child: Row(
+            //         mainAxisAlignment: MainAxisAlignment.center,
+            //         children: [
+            //           const SizedBox(
+            //             width: 10,
+            //           ),
+            //           SizedBox(
+            //             width: 40,
+            //             child: TextField(
+            //               controller: countryCode,
+            //               keyboardType: TextInputType.phone,
+            //               decoration:
+            //                   const InputDecoration(border: InputBorder.none),
+            //             ),
+            //           ),
+            //           const Text('|',style: TextStyle(fontSize: 33,color: Colors.grey),),
+            //
+            //           const SizedBox(width: 10,),
+            //           Expanded(child: TextField(
+            //             onChanged: (value){
+            //               phone=value;
+            //             },
+            //              keyboardType: TextInputType.phone,
+            //             decoration: const InputDecoration(
+            //               border: InputBorder.none,
+            //               hintText: "Phone",
+            //
+            //             ),
+            //           ))
+            //         ],
+            //       ),
+            //     )),
             Container(
               padding: const EdgeInsets.all(16.0),
               width: size.width,
               child: ElevatedButton.icon(
                 style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                onPressed: () async {
-                  await FirebaseAuth.instance.verifyPhoneNumber(
-                    phoneNumber: countryCode.text+phone,
-                    verificationCompleted: (PhoneAuthCredential credential) {},
-                    verificationFailed: (FirebaseAuthException e) {},
-                    codeSent: (String verificationId, int? resendToken) {
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>PhoneScreen()));
-                    },
-                    codeAutoRetrievalTimeout: (String verificationId) {},
-                  );
-
-                },
+                  onPressed: (){
+                    if(otpFieldVisibility){
+                      verifyOTPCode();
+                    } else{
+                      verifyUserPhoneNumber();
+                    }
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  },
                 icon: const Icon(Icons.phone),
                 label: const Text("LOGIN WITH PHONE"),
               ),
@@ -95,4 +94,48 @@ class _PhoneScreenState extends State<PhoneScreen> {
       ),
     );
   }
+
+  Future<void> verifyOTPCode() async {
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(
+      verificationId: receivedID,
+      smsCode: countryCode.text,
+    );
+    await auth
+        .signInWithCredential(credential)
+        .then((value) => print('User Login In Successful'));
+  }
+
+  void verifyUserPhoneNumber()async{
+    await auth.verifyPhoneNumber(
+      phoneNumber: countryCode.text+phone,
+      verificationCompleted: (PhoneAuthCredential credential) async{
+        await auth.signInWithCredential(credential).then(
+              (value) => print('Logged In Successfully'),
+        );
+      },
+      verificationFailed: (FirebaseAuthException e) {
+        print(e.message);
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        receivedID = verificationId;
+
+        setState(() {});
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>const PhoneScreen()));
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        print('TimeOut');
+      },
+    );
+  }
+
+   // processLogin(BuildContext context) {
+   //
+   //  var user=FirebaseAuth.instance.currentUser;
+   //
+   //  if(user==null){
+   //    FirebaseAuthUi.instance().launchAuth([AuthProvider.phone()]).then((firebaseUser){
+   //
+   //    });
+   //  }
+   // }
 }
